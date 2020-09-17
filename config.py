64b102553429312704452e5a1c14fb6145d0ff21
Jetsukda Janthornthao <53548542+Jetsukda@users.noreply.github.com>
2020-09-17 12:38:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,6 @@ user_list = {"1": {"username": "xxxxxxx",
 url_google_sheet = "https://docs.google.com/spreadsheets/d/1YUzuWOA3E9Frpjn1UzfFuVxOcodCKnx57p8qdYJm0C8" \
                    "/edit?usp=sharing"
 
-url_website = 'https://www.lotto.ktbnetbank.com/KTBLotto/?fbclid=IwAR1UN7T8ah93' \
-              'hqauUTL5PCUmUEVh0VspkM9vd3nKqYrzicY-EV_4An1hrIY#/login'
+url_website = 'ใส่ url'
 
-open_script = "window.open('https://www.lotto.ktbnetbank.com/KTBLotto/?fbclid=IwAR3CizKucdT0--b" \
-              "tFPxIm2zKWDRCXdlAnAQZRRnbUXsEBmdlyZ2j-CJ3410#/login')"
+open_script = "window.open('ใส่ url')"
